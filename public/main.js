89c568d6bb16867060f2973450adb42ee4adfff0
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-10-06 06:59:26 -0300

    init server with node and express

---
+++ public/main.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const recipes = document.querySelectorAll('.recipe')
+
+for (let recipe of recipes) {
+    recipe.addEventListener('click', () => {
+        const imgPath = recipe.getAttribute('id')
+        const titleRecipe = recipe.querySelector('h2').textContent
+        const authorRecipe = recipe.querySelector('p').textContent
+
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `../assets/${imgPath}.png`
+        modalOverlay.querySelector('h2').textContent = titleRecipe
+        modalOverlay.querySelector('p').textContent = authorRecipe
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+    modalOverlay.classList.remove('active')
+})
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+server.use(express.static('assets'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get('/', (req, res) => {
-    res.send('oi')
+    return res.render('index')
 })
 
 server.listen(3000, () => {
-    console.log("Server online on port 6000");
+    console.log("Server online on port 3000");
 })
