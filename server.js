67be659f2b320854b422592ac5876701e2f0da3b
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-10-09 08:07:04 -0300

    create server and list recipes whit nodejs express

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const data = require('./data')
 
 const server = express()
 
 server.use(express.static('public'))
 server.use(express.static('assets'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
+
+const dataHome = data.slice(0, 3)
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get('/', (req, res) => {
     return res.render('index')
+})
+
+server.get('/about.html', (req, res) => {
+    return res.render('about')
+})
+
+server.get('/recipes.html', (req, res) => {
+    return res.render('recipes', { items: data })
+})
+
+server.get('/index.html', (req, res) => {
+    return res.render('index', { items: dataHome })
 })
 
 server.listen(3000, () => {
